45b4251a5745639e20554819e6537b05f7ea4b91
Joseph Abdnor <jabdnor@mail.usf.edu>
2011-09-21 13:22:46 -0400

    Tag

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Scraper1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter URL: ");
+            String URL = Console.ReadLine();
+
+            WebRequest req = WebRequest.Create(URL);
+            WebResponse resp = req.GetResponse();
+
+            StreamReader reader =
+                new StreamReader(resp.GetResponseStream(), Encoding.ASCII);
+
+            String page = reader.ReadToEnd();
+            Console.WriteLine(page);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sup/sup/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace csharp_station.howto
+{
+    class TextFileWriter
+    {
+        static void Main(string[] args)
+        {
+            // create a writer and open the file
+            TextWriter tw = new StreamWriter(@"C:\Users\Joe\Desktop\Test.xml");
+
+            Console.WriteLine("Enter Website name");
+            string web = Console.ReadLine();
+
+            Console.WriteLine("Was the finding a success");
+            string suc = Console.ReadLine();
+
+
+            // write a line of text to the file
+            tw.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            tw.WriteLine("<Test>");
+            tw.WriteLine("  <website> " + web + " </website>");
+            tw.WriteLine("  <success> " + suc + " </success>");
+            tw.WriteLine("</Test>");
+
+            // close the stream
+            tw.Close();
+        }
+    }
+}
